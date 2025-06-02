57905f9d64d5785b477dc5663d525ad31ee94108
fg_daemon <67126122+ostronic@users.noreply.github.com>
2025-06-02 10:25:23 -0400

    Update netck.py

--- datasaver/netck.py
+++ datasaver/netck.py
@@ -4,7 +4,7 @@ import sys, subprocess
 
 def Netfunc():
     ''' We are only checking for the return code, in which case if it is not 0(successful ping of count 10), we print a prompt status to the screen.'''
-    ostronics = subprocess.call(['ping', 'google.com', '-c', '3'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    ostronics = subprocess.call(['ping', 'google.com', '-c', '1'] stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if ostronics != 0:
         print('CHECKi YOUR NETWORK CONNECTION, VPN, OR CONNECT TO A WIFI NETWORK!!! \n TO START DATASAVER')
         sys.exit(1)
